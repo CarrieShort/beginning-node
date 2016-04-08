3b0ae869190b20e3223a613a77c6b9a3f2a12236
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-08 13:10:35 -0700

    add request_handlers

---
+++ request_handlers.js
@@ -0,0 +1,10 @@
+function start(){
+  console.log('Rquest handler \'sart\' was called.');
+}
+
+function upload(){
+  console.log('Request handler \'upload\' was called.');
+}
+
+exports.start = start;
+exports.upload = upload;
