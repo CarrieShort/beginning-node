f6e68e5c0580a02a26df4327b778e3362c5abc7d
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-08 21:45:56 -0700

    use query string to improve text display, spaces instead of +

--- request_handlers.js
+++ request_handlers.js
@@ -1,7 +1,7 @@
-var exec = require('child_process').exec;
+var querystring = require('querystring');
 
 function start(response, postData){
-  console.log('Rquest handler \'sart\' was called.');
+  console.log('Request handler \'sart\' was called.');
 
   var body = '<html>'+
     '<head>' +
@@ -24,7 +24,7 @@ function start(response, postData){
 function upload(response, postData){
   console.log('Request handler \'upload\' was called.');
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('You\'ve sent: ' + postData);
+  response.write('You\'ve sent the text: ' + querystring.parse(postData).text);
   response.end();
 }
 
